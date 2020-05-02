dd3943001208cd3e08712bf432c11fc3ec55c33b
Juwita14 <56763570+Juwita14@users.noreply.github.com>
2020-05-02 18:12:18 +0700

    Update ssfs.c

--- ssfs.c
+++ ssfs.c
@@ -166,7 +166,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset, stru
 
 	(void) fi;
 
-	fd = open(path, O_RDONLY);
+	fd = open(fpath, O_RDONLY);
 	if (fd == -1)
 		return -errno;
 	res = pread(fd, buf, size, offset);
